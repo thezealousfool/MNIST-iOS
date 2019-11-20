2e0844d259c5c81b21c780d2562d732fd57fece2
Vivek Roy <thezealousfool@gmail.com>
2019-11-20 01:52:10 -0500

    Initial Commit

---
+++ NavCog DNN/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavCog DNN
+//
+//  Created by Vivek Roy on 11/20/19.
+//  Copyright © 2019 Vivek Roy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
